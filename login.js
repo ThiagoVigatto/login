23c41461a3187e3b7a6dc2f9147b7f844e436f1d
ThiagoVigatto <135845694+ThiagoVigatto@users.noreply.github.com>
2023-09-20 13:34:20 -0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,14 @@
+username == "rafaum";
+password == "123456";
+
+function autenticar(){
+    var lgn = document.getElementById('login').value;
+    var pwd = document.getElementById('passwd').value;
+
+if
+    (lgn == username & pwd == password)
+    alert('Seus dados estão corretos')
+else{
+    alert('Seus dados estão incorretos');
+    }
+}
